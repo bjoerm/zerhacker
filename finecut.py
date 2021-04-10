14418e51c165a1f3cc16e256ce9878b0f92fe9aa
bjoerm <bjoern-menke@web.de>
2021-04-10 16:53:30 +0200

    Fixed handling of subfolders.

--- finecut.py
+++ finecut.py
@@ -3,11 +3,10 @@
 import cv2
 import numpy as np
 import os
-import glob
 from multiprocessing import Pool
-import multiprocessing
 from pathlib import Path
 from shared_utils import SharedUtility
+from shutil import copyfile
 
 
 class FineCut:
@@ -22,14 +21,14 @@ class FineCut:
 
         Number of threads to be used to process the images in parallel. If not provided, the script will try to find the value itself (which doesn't work on Windows or MacOS -> defaults to 1 thread only).
         """
+        # TODO DELETE???
+        # input_path = Path(parent_path_images) / Path(input_path)
+        # output_path = Path(parent_path_images) / Path(output_path)
 
-        input_path = Path(parent_path_images) / Path(input_path)
-        output_path = Path(parent_path_images) / Path(output_path)
-
-        files = SharedUtility.generate_file_list(path=input_path)
+        files = SharedUtility.generate_file_list(path=Path(parent_path_images) / Path(input_path))
 
         if len(files) == 0:
-            print(f"No image files found in {input_path}\n Exiting.")  # TODO Add stop of the programm.
+            print(f"No image files found in {Path(parent_path_images) / Path(input_path)}\n Exiting.")  # TODO Add stop of the programm.
 
         else:
             # Creating list of dictionaries for parallel processing.
@@ -37,10 +36,11 @@ class FineCut:
 
             for f in files:
                 params.append({
-                    "thresh": thresh
-                    , "crop": extra_crop
-                    , "filename": f
+                    "input_image_path": f
+                    , "input_path": input_path
                     , "output_path": output_path
+                    , "thresh": thresh
+                    , "crop": extra_crop
                     })
 
             # Parallel fine cut of the scanned images.
@@ -51,14 +51,14 @@ class FineCut:
 
     @classmethod
     def autocrop(cls, params: dict):
+        input_image_path = params["input_image_path"]
+        input_path = params["input_path"]
+        output_path = params["output_path"]
         thresh = params["thresh"]
         crop = params["crop"]
-        filename = params["filename"]
-        output_path = params["output_path"]
 
-        print(f"Opening: {filename}")
-        name = Path(filename).name
-        img = cv2.imread(filename)
+        name = Path(input_image_path).name
+        img = cv2.imread(input_image_path)
 
         # Add white background (in case one side is cropped right already, otherwise script would fail finding contours)
         img = cv2.copyMakeBorder(src=img, top=100, bottom=100, left=100, right=100, borderType=cv2.BORDER_CONSTANT, value=[255, 255, 255])
@@ -67,27 +67,31 @@ class FineCut:
         res_gray = cv2.resize(src=img, dsize=(int(im_w / 6), int(im_h / 6)), interpolation=cv2.INTER_CUBIC)  # TODO What is this need for? Try to understand before dropping it?
         found, img = cls.cont(img=img, gray=gray, user_thresh=thresh, crop=crop)
 
+        output_image_path = ""
+
         if found:
-            print(f"Saving to: {output_path}/{name}")
-            try:
-                cv2.imwrite(f"{output_path}/{name}", img, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
-            except:
-                None
-            # TODO: This is always writing JPEG, no matter what the input file type was. Nice to have: Change this.
+            # Create folders, if not exists. (Is required if there are folders in the input.)
+            output_image_path = str(input_image_path).replace(input_path, output_path)
+            Path(output_image_path).parent.mkdir(parents=True, exist_ok=True)
+            cv2.imwrite(output_image_path, img, [int(cv2.IMWRITE_JPEG_QUALITY), 95])  # TODO: This is always writing JPEG, no matter what the input file type was. Nice to have: Change this.
 
         else:
             # If no contours were found, write input file to "failed" folder
-            print(f"Failed finding any contour. Saving original file to {output_path}/failed/{name}")
-            if not os.path.exists(f"{output_path}/failed/"):
-                os.makedirs(f"{output_path}/failed/")
+            output_image_path = str(input_image_path).replace(input_path, output_path + "/failed/")
+            Path(output_image_path).parent.mkdir(parents=True, exist_ok=True)
 
-            with open(filename, "rb") as in_f, open(f"{output_path}/failed/{name}", "wb") as out_f:
-                while True:
-                    buf = in_f.read(1024**2)
-                    if not buf:  # TODO Understand this. When will this be triggered?
-                        break
-                    else:
-                        out_f.write(buf)
+            copyfile(input_image_path, output_image_path)
+
+            # if not os.path.exists(f"{output_path}/failed/"):
+            #     os.makedirs(f"{output_path}/failed/")
+
+            # with open(filename, "rb") as in_f, open(f"{output_path}/failed/{name}", "wb") as out_f:
+            #     while True:
+            #         buf = in_f.read(1024**2)
+            #         if not buf:  # TODO Understand this. When will this be triggered?
+            #             break
+            #         else:
+            #             out_f.write(buf)
 
     def order_rect(points):
         # idea: https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
@@ -160,17 +164,18 @@ class FineCut:
                     epsilon = 0.1 * cv2.arcLength(curve=cnt, closed=True)
                     approx = cv2.approxPolyDP(curve=cnt, epsilon=epsilon, closed=True)
 
+                    # TODO Understand the following part!
                     if len(approx) == 4:
                         found = True
                     elif len(approx) > 4:
                         user_thresh = user_thresh - 1
-                        print(f"Adjust Threshold: {user_thresh}")
+                        # print(f"Adjust Threshold: {user_thresh}")  # Commented this out to not get spammed when processing many files.
                         if user_thresh == old_val + 1:
                             loop = True
                         break
                     elif len(approx) < 4:
                         user_thresh = user_thresh + 5
-                        print(f"Adjust Threshold: {user_thresh}")
+                        # print(f"Adjust Threshold: {user_thresh}")  # Commented this out to not get spammed when processing many files.
                         if user_thresh == old_val - 5:
                             loop = True
                         break
@@ -189,14 +194,16 @@ class FineCut:
                         # if this happens a lot, increase the threshold, maybe it helps, otherwise just stop
                         user_thresh = user_thresh + 5
                         if user_thresh > 255:
+                            print("WARNING: This seems to be an edge case. If the result isn't satisfying try lowering the threshold using -t")
                             break
-                        print(f"Adjust Threshold: {user_thresh}")
-                        print("WARNING: This seems to be an edge case. If the result isn't satisfying try lowering the threshold using -t")
+
+                        # print(f"Adjust Threshold: {user_thresh}")
+
 
                         if user_thresh == old_val - 5:
                             loop = True
             i += 1
             if i % 2 == 0:
-                old_value = user_thresh  # TODO What is this part for?
+                old_val = user_thresh  # TODO What is this part for?
 
         return found, img
