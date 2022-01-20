35aa5f6e24f3dbbe11dfbbda09619d851553c719
bjoerm <bjoern-menke@web.de>
2022-01-20 21:57:31 +0100

    Using black autoformating.

--- __main__.py
+++ __main__.py
@@ -18,13 +18,32 @@ def main():
     # Load options
     cfg = toml.load("options.toml", _dict=dict)
 
-    Environment.initiate(cfg.get("parent_path_images"), cfg.get("untouched_scans_path"), cfg.get("rough_cut_path"), cfg.get("fine_cut_path"))
+    Environment.initiate(
+        cfg.get("parent_path_images"),
+        cfg.get("untouched_scans_path"),
+        cfg.get("rough_cut_path"),
+        cfg.get("fine_cut_path"),
+    )
 
     cfg["num_threads"] = SharedUtility.get_available_threads()
 
-    Splitter.main(parent_path_images=cfg.get("parent_path_images"), input_path=cfg.get("untouched_scans_path"), output_path=cfg.get("rough_cut_path"), min_pixels=cfg.get("min_pixels"), detection_threshold=cfg.get("detection_threshold"), num_threads=cfg.get("num_threads"))
+    Splitter.main(
+        parent_path_images=cfg.get("parent_path_images"),
+        input_path=cfg.get("untouched_scans_path"),
+        output_path=cfg.get("rough_cut_path"),
+        min_pixels=cfg.get("min_pixels"),
+        detection_threshold=cfg.get("detection_threshold"),
+        num_threads=cfg.get("num_threads"),
+    )
 
-    FineCut.main(parent_path_images=cfg.get("parent_path_images"), input_path=cfg.get("rough_cut_path"), output_path=cfg.get("fine_cut_path"), thresh=cfg.get("detection_threshold_finecut"), extra_crop=cfg.get("extra_crop"), num_threads=cfg.get("num_threads"))
+    FineCut.main(
+        parent_path_images=cfg.get("parent_path_images"),
+        input_path=cfg.get("rough_cut_path"),
+        output_path=cfg.get("fine_cut_path"),
+        thresh=cfg.get("detection_threshold_finecut"),
+        extra_crop=cfg.get("extra_crop"),
+        num_threads=cfg.get("num_threads"),
+    )
 
 
 if __name__ == "__main__":
