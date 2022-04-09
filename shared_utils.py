aea3e55ccd14f79644ff07aa2d7165c1f4a2f070
bjoerm <bjoern-menke@web.de>
2022-04-09 23:32:43 +0200

    Added writing.

--- shared_utils.py
+++ shared_utils.py
@@ -1,14 +1,15 @@
-import glob
 import os
 from pathlib import Path
+import numpy as np
+import cv2
 
 
 class SharedUtility:
-    """ This class contains shared utility methods. """
+    """This class contains shared utility methods."""
 
     @staticmethod
     def generate_file_list(path: str) -> list:
-        """ Generate a list of all images in the input path. """
+        """Generate a list of all images in the input path."""
 
         file_list_gen = Path(path)
         file_list = [str(f.parent / f.name) for f in file_list_gen.rglob("*.jpg")]  # rglob is recursive. # TODO Deal with .jpeg, .JPG, ...
@@ -26,7 +27,7 @@ class SharedUtility:
 
     @staticmethod
     def get_available_threads() -> int:
-        """ Get number of available CPU threads that can be used for parallel processing. """
+        """Get number of available CPU threads that can be used for parallel processing."""
 
         try:
             num_threads = os.cpu_count()  # This is not 100% the best approach but os.sched_getaffinity does not work on Windows.
@@ -38,3 +39,19 @@ class SharedUtility:
         print(f"\n[Status] Using {num_threads} threads.")
 
         return num_threads
+
+    @staticmethod
+    def save_image(image: np.ndarray, output_path: Path, jpg_quality: int):
+        """
+        Saving image. imwrite does not work with German Umlaute and other special characters. Thus, the following solution.
+        Encode the im_resize into the im_buf_cropped, which is a one-dimensional ndarray (from https://jdhao.github.io/2019/09/11/opencv_unicode_image_path/#write-images-with-unicode-paths)
+        """
+
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+
+        is_success, im_buf_cropped = cv2.imencode(".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), jpg_quality])
+
+        if is_success is True:
+            im_buf_cropped.tofile(str(output_path))
+        else:
+            raise ValueError("Errer when writing file.")
